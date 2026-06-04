bb03193066585f72ac99a6be1a63a2fbd78ad023
Pooja Thakur <poojauthakur00@gmail.com>
2026-06-04 00:28:38 -0400

    Deploy on Render

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 import { router as enrichRouter } from "./routes/enrich.js";
 import { migrate } from "./migrate.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", enrichRouter);
+
+// Serve the built React app in production.
+app.use(express.static(join(__dirname, "../dist")));
+app.get("*", (_req, res) => res.sendFile(join(__dirname, "../dist/index.html")));
 
 const PORT = process.env.PORT || 3001;
 
